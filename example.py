a583d5c98c4a36deb008b1bbd8d2aa8731a45ddc
Zsolt Janosi <zsolt.janosi@3ss.tv>
2018-08-18 13:18:37 +0300

    tsplib

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xml.etree.ElementTree as ET
 
 from ant_colony import AntColony
 
@@ -7,6 +8,15 @@ distances = np.array([[np.inf, 2, 2, 5, 7],
                       [2, 4, np.inf, 1, 3],
                       [5, 8, 1, np.inf, 2],
                       [7, 2, 3, 2, np.inf]])
+
+
+def loadTsp (name='gr21.xml'):
+    tree = ET.parse(name)
+    root = tree.getroot()
+
+    distances = np.empty([[]])
+    return distances
+
 
 ant_colony = AntColony(distances, 1, 1, 100, 0.95, alpha=1, beta=1)
 shortest_path = ant_colony.run()
